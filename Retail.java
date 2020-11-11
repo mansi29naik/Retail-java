a66cbf59104626632ff8c64c11fa1dec3780dd79
mansi29naik <36603771+mansi29naik@users.noreply.github.com>
2020-11-11 13:44:56 -0500

    Add files via upload
    It will be better if the Java code is written in NetBeans to easily connect the database

---
+++ Retail.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util. * ;
+import java.sql. * ;
+
+public class Retail {
+  private static Connection conn; //connection
+  private static Driver driver; //driver
+  private static Statement statement; //statement
+  private static ResultSet result; // SQL command result
+  private static String str; // user-entered SQL command (ASCII text)
+  private static Enumeration < Driver > dEnum; // debug
+  private static String classpath; // debug
+  private static Scanner scan = new Scanner(System. in ); // for user input
+ 
+  public static void main(String[] args) throws ClassNotFoundException,FileNotFoundException,SQLException 
+  {
+
+    
+  }
+}
